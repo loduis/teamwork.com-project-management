d28c9e730415105bef742e8849d7e210c00bb48f
Loduis Madariaga <loduis@gmail.com>
2018-10-06 08:37:02 -0500

    add resource company, task , taskList.

---
+++ src/Company.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace TeamWorkPm;
+
+final class Company extends Resource
+{
+
+}
---
+++ src/TaskList.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace TeamWorkPm;
+
+final class TaskList extends Resource
+{
+    protected static $path = 'tasklists';
+}
